5abc3da84d07552c9a237f3de6fbb75569d227d7
thxxx <zxcv05999@naver.com>
2020-08-22 00:21:54 +0900

    08.21

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://Alonso:1234qwer@boilerplate.nmrsc.mongodb.net/<dbname>?retryWrites=true&w=majority", {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false //에러가 안뜨게 하려고
+mongoose.connect('mongodb+srv://Alonso:1234qwer@boilerplate.nmrsc.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify: false //에러가 안뜨게 하려고
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err=> console.log(err))
+  .catch(err=> console.log(console.error))
 
 
 app.get('/', (req,res) => res.send('Hello Node World!') )
 
-app.listen(port, () => console.log('`Example app listening on port ${port}!`'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
